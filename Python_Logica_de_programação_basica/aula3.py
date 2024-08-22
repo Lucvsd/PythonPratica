cbbd0c01c8f98046ded06297b02a1ccfa4e023d7
Lucas Pontes <89760771+Lucvsd@users.noreply.github.com>
2024-08-22 15:12:43 -0300

    Add files via upload

---
+++ Python_Logica_de_programação_basica/aula3.py
@@ -0,0 +1,23 @@
+"""
+DocSring
+Python = linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto 
+String são textos que estão dentro de aspas
+"""
+
+print(1234)
+
+# Aspas simpes
+print('Lucas Pontes')
+print(1, 'Lucas "Pontes"')
+
+# Aspas duplas
+print("Lucas Pontes")
+print(2, "Lucas 'Pontes'")
+
+# Escape
+print("Lucas \"Pontes\"")
+
+# r
+print(r"Lucas \"Pontes\"")
